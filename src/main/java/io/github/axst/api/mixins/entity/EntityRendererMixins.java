13497692b6d34c2c2332ddbe8b58555ceaf45cd4
xNtrow <xntrowdev@gmail.com>
2022-08-03 14:03:56 +0200

    Update

--- src/main/java/io/github/axst/api/mixins/entity/EntityRendererMixins.java
+++ src/main/java/io/github/axst/api/mixins/entity/EntityRendererMixins.java
@@ -1,8 +1,6 @@
 package io.github.axst.api.mixins.entity;
 
-import io.github.axst.Limee;
 import io.github.axst.impl.awt.notifications.Notification;
-import io.github.axst.impl.events.Render3DEvent;
 import net.minecraft.client.renderer.EntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,12 +9,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixins {
-
-    @Inject(method = "renderWorldPass", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/EntityRenderer;renderHand:Z", shift = At.Shift.BEFORE))
-    private void renderWorldPass(int pass, float partialTicks, long finishTimeNano, CallbackInfo callbackInfo) {
-        Render3DEvent event = new Render3DEvent(partialTicks);
-        Limee.getInstance().getBus().post(event);
-    }
 
     @Inject(method = "updateCameraAndRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiScreen;drawScreen(IIF)V", shift = At.Shift.AFTER))
     private void updateCameraAndRender(float partialTicks, long nanoTime, CallbackInfo callbackInfo) {
